ea94cdf59e59fa3983bff2da0ca8b088d3891141
Joon <joonlee4620@gmail.com>
2023-02-24 13:58:31 -0500

    initialState AppContext.js

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -1,1 +1,12 @@
 import React, { createContext, useReducer } from 'react';
+
+const initialState = {
+    expenses: [
+        {id: "Shirt", name: 'Shirt', quantity: 0, unitprice: 500 },
+        {id: "Jeans", name: 'Jeans', quantity: 0, unitprice: 300 },
+        {id: "Dress", name: 'Dress', quantity: 0, unitprice: 400 },
+        {id: "Dinner set", name: 'Dinner set', quantity: 0, unitprice: 600 },
+        {id: "Bags", name: 'Bags', quantity: 0, unitprice: 200}
+    ],
+    Location: 'E'
+};
